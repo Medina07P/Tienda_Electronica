a797781c31ebd2bd945948513850a3f38335a191
1002805933 <1002805933@PC5SJ209.fup.edu>
2024-04-10 11:32:12 -0500

    Ultima actualización

--- src/main/java/Helpers/HelperRegistro.java
+++ src/main/java/Helpers/HelperRegistro.java
@@ -4,10 +4,41 @@
  */
 package Helpers;
 
+import Logica_Conexion.PersonaProvider;
+import Logica_Negocio.Persona;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author 1002805933
  */
 public class HelperRegistro {
+    public static void RegistrarPersonaNube(Persona objper, int id, String producto)
+    {
+       boolean res= PersonaProvider.RetornarUid(objper.getUid());
+       if(!res){
+           try {
+               Map<String, Object> datos= new HashMap<>();
+               datos.put("uid", objper.getUid());
+               datos.put("Nombre", objper.getNombre());
+               datos.put("Apellido", objper.getApellido());
+               datos.put("Direccion", objper.getDireccion());
+               datos.put("Cedula", objper.getCedula());
+               datos.put("Productos", objper.getProductos());
+               datos.put("Nom_img", objper.getNom_img());
+               long inicio= System.currentTimeMillis();
+               PersonaProvider.GuardarPersona("Persona", String.valueOf(id),datos);
+               long fin= System.currentTimeMillis();
+               HelperTiempo.RetornarTiempo(fin, inicio);
+               System.out.println("Persona guardada con exito con id"+"\t"+id);
+           }catch (Exception e) {
+               System.out.println("Errro"+ e.getMessage());
+           }
+       }else {
+           System.out.println("El uid ya existe");
+       }
+        
+    }
     
 }
--- src/main/java/Logica_Conexion/PersonaProvider.java
+++ src/main/java/Logica_Conexion/PersonaProvider.java
@@ -4,10 +4,95 @@
  */
 package Logica_Conexion;
 
+import Logica_Negocio.Persona;
+import Logica_Negocio.Producto;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.cloud.firestore.WriteResult;
+import com.google.firebase.cloud.FirestoreClient;
+import java.util.ArrayList;
+import java.util.Map;
+
 /**
  *
  * @author 1002805933
  */
 public class PersonaProvider {
-    
+
+    CollectionReference reference;
+    public static Firestore db;
+
+    public static boolean GuardarPersona(String coleccion, String documento, Map<String, Object> data) {
+
+        db = FirestoreClient.getFirestore();
+
+        try {
+            DocumentReference docRef = db.collection(coleccion).document(documento);
+            ApiFuture<WriteResult> result = docRef.set(data);
+            System.out.println("Guardando Correctamente");
+            return true;
+
+        } catch (Exception e) {
+            System.out.println("Error" + e.getMessage());
+        }
+        return false;
+    }
+
+    public static ArrayList CargarInfoPersona() {
+        Persona objper;
+        Producto produ;
+        ArrayList<Persona> lspersona = new ArrayList<>();
+        ArrayList<Persona> lsprodu = new ArrayList<>();
+
+        try {
+            CollectionReference persona = Conexion.db.collection("Persona");
+            ApiFuture<QuerySnapshot> querySnap = persona.get();
+            for (DocumentSnapshot document : querySnap.get().getDocuments()) {
+                objper = new Persona(document.getString("uid"),
+                        document.getString("Nombre"),
+                        document.getString("Apellido"), 
+                        document.getString("Cedula"), 
+                        document.getString("Direccion"),
+                        document.getString("Productos"), 
+                        document.getString("Nom_img")
+                );
+                lspersona.add(objper);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error"+ e.getMessage());
+        }
+        return lspersona;
+    }
+
+    public static boolean RetornarUid(String uid){
+        ArrayList<String> uids = new ArrayList<>();
+        boolean rta= true;
+        try {
+            CollectionReference persona= Conexion.db.collection("Persona");
+            ApiFuture<QuerySnapshot> querySnap= persona.get();
+            for (DocumentSnapshot document: querySnap.get().getDocuments()){
+                uids.add(document.getString("uid"));
+                
+                
+            }
+            for (int i = 0; i < uids.size(); i++) {
+                if (uid.equals(uids.get(i)))
+                {
+                    return rta;
+                }
+                else {
+                    return !rta;
+                }
+                
+            }
+        } catch (Exception e) {
+            System.out.println("Error"+ e.getMessage());
+        }
+        return rta;
+    }
 }
