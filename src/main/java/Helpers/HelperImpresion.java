e38ab1de17a4629d9781f7ddb0d7bdd873bc1c0a
1002805933 <1002805933@PC5SJ209.fup.edu>
2024-04-17 10:37:28 -0500

    Ultima actualización Tienda Electrónica

--- src/main/java/Helpers/HelperImpresion.java
+++ src/main/java/Helpers/HelperImpresion.java
@@ -4,10 +4,83 @@
  */
 package Helpers;
 
+import Logica_Conexion.Conexion;
+import Logica_Negocio.Persona;
+import Logica_Negocio.Producto;
+import com.google.cloud.firestore.CollectionReference;
+import java.util.ArrayList;
+
 /**
  *
  * @author 1002805933
  */
 public class HelperImpresion {
-    
+
+    public static void ImprimirInfoPersonaNube(ArrayList<Persona> lspersonasnube) {
+        String[] parts = null,
+                parts2 = null;
+        Producto objprodu;
+        ArrayList<Producto> lsnube = new ArrayList<>();
+
+        for (int i = 0; i < lspersonasnube.size(); i++) {
+            System.out.println("Persona" + "\t" + (i + 1) + "\n"
+                    + "El id de la persona es: " + lspersonasnube.get(i).getUid() + "\n"
+                    + "El nombre de la persona es: " + lspersonasnube.get(i).getNombre()
+                    + "El apellido de la persona es: " + lspersonasnube.get(i).getApellido()
+                    + "La cedula de la persona es: " + lspersonasnube.get(i).getCedula()
+                    + "La direccion de la persona es: " + lspersonasnube.get(i).getDireccion());
+            parts = lspersonasnube.get(i).getProducto().split(";");
+            for (int j = 0; j < parts.length; j++) {
+                parts2 = parts[j].split(",");
+                objprodu = new Producto(parts2[0], parts2[1], parts2[2]);
+                lsnube.add(objprodu);
+            }
+            for (int k = 0; k < lsnube.size(); k++) {
+                System.out.println("Producto: " + "\t" + (k + 1));
+                System.out.println("Nombre: " + "\t" + lsnube.get(k).getNombre());
+                System.out.println("Marca: " + "\t" + lsnube.get(k).getMarca());
+                System.out.println("Serial: " + "\t" + lsnube.get(k).getSerial());
+            }
+        }
+
+    }
+
+    public static void BuscarPersonaNube(ArrayList<Persona> lspersonasnube, String codigo) {
+        String[] partes = null,
+                partes2 = null;
+
+        int bandera = 0;
+        Producto objprodu;
+        ArrayList<Producto> lsnube = new ArrayList<>();
+
+        for (int i = 0; i < lspersonasnube.size(); i++) {
+            if (codigo.equals(lspersonasnube.get(i).getUid())) {
+                bandera = 1;
+                System.out.println("Persona" + "\t" + (i + 1) + "\n"
+                        + "El id de la persona es: " + lspersonasnube.get(i).getUid() + "\n"
+                        + "El nombre de la persona es: " + lspersonasnube.get(i).getNombre() + "\n"
+                        + "El apellido de la persona es: " + lspersonasnube.get(i).getApellido() + "\n"
+                        + "La cedula de la persona es: " + lspersonasnube.get(i).getCedula() + "\n"
+                        + "La direccion de la persona es: " + lspersonasnube.get(i).getDireccion());
+                partes = lspersonasnube.get(i).getProducto().split(";");
+                for (int j = 0; j < partes.length; j++) {
+                    partes2 = partes[j].split(",");
+                    objprodu = new Producto(partes2[0], partes2[1], partes2[2]);
+                    lsnube.add(objprodu);
+                }
+                for (int k = 0; k < lsnube.size(); k++) {
+                    System.out.println("Producto: " + "\t" + (k + 1));
+                    System.out.println("Nombre: " + lsnube.get(i).getNombre());
+                    System.out.println("Marca: " + lsnube.get(i).getMarca());
+                    System.out.println("Serial: " + lsnube.get(i).getSerial() + "\n");
+                }
+                lsnube.clear();
+                partes = null;
+                partes2 = null;
+            }
+        }
+        if (bandera == 0) {
+            System.out.println("El uid buscado no se encuentra en la lista");
+        }
+    }
 }
